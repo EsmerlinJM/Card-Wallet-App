9fb02adb02ef359e7beabe102112a52b3e3acdfd
Levi Lesches <levilesches@gmail.com>
2019-08-20 19:34:09 -0400

    Resized CardList

--- lib/ui/widgets/card_list.dart
+++ lib/ui/widgets/card_list.dart
@@ -1,8 +1,10 @@
-import '../../blocs/card_list_bloc.dart';
 import 'package:flutter/material.dart';
-import '../../models/card_model.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import './card_chip.dart';
+
+import '../../blocs/card_list_bloc.dart';
+import '../../models/card_model.dart';
+import 'card_chip.dart';
+import "card_logo.dart";
 
 class CardList extends StatelessWidget{
   @override
@@ -12,27 +14,27 @@ class CardList extends StatelessWidget{
     return StreamBuilder<List<CardResults>>(
       stream: cardListBloc.cardList,
       builder: (context, snapshot){
-        return Column(
-          children: <Widget>[
-            !snapshot.hasData 
-              ? CircularProgressIndicator() 
-              : SizedBox(
-                height: _screenSize.height * 0.8,
-                child: Swiper(
-                  itemBuilder: (BuildContext context, int index){
-                    return CardFromList(
-                      cardModel: snapshot.data[index],
-                    );
-                  },
-                  itemCount: snapshot.data.length,
-                  itemWidth: _screenSize.width * 0.61,
-                  itemHeight: _screenSize.height * 0.52,
-                  layout: SwiperLayout.STACK,
-                  scrollDirection: Axis.vertical,
-                ),
-              )  
-          ],
-        );
+        return !snapshot.hasData 
+          ? CircularProgressIndicator() 
+          : SizedBox(
+            height: _screenSize.height * 0.8,
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index){
+                return CardFromList(
+                  cardModel: snapshot.data[index],
+                );
+              },
+              itemCount: snapshot.data.length,
+              itemWidth: _screenSize.width * 0.6,
+
+              itemHeight: _screenSize.height * (
+                MediaQuery.of(context).orientation == Orientation.portrait 
+                  ? 0.52 : 0.6
+              ),
+              layout: SwiperLayout.STACK,
+              scrollDirection: Axis.vertical,
+            ),
+          );  
       },
     );
   }
@@ -40,142 +42,102 @@ class CardList extends StatelessWidget{
 
 // Crear en un nuevo archivo e importarlo
 class CardFromList extends StatelessWidget {
+  static const Widget dotPadding = SizedBox(width: 30);
+  static final Widget dot = Padding (
+    padding: EdgeInsets.symmetric(horizontal: 3),
+    child: Text (
+      "•",
+      textScaleFactor: 2,
+      style: TextStyle(
+        color: Colors.white, 
+        fontWeight: FontWeight.bold
+      ),
+    )
+  );
+
   final CardResults cardModel;
 
-  CardFromList({this.cardModel});
+  const CardFromList({this.cardModel});
 
   @override
-  Widget build(BuildContext context) {
-    final _cardLogo = Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 25.0, right: 52.0),
-          child: Image(
-            image: AssetImage('assets/visa_logo.png'),
-            width: 65.0,
-            height: 32.0,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(right: 52.0),
-          child: Text(
-            cardModel.cardType,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 14.0,
-                fontWeight: FontWeight.w400
-            ),
-          ),
-        )
-      ],
-    );
-
-    final _cardNumber = Padding(
-      padding: const EdgeInsets.only(top: 15.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          _buildDots()
-        ],
-      ),
-    );
-
-    final _cardLastNumber = Padding(
-      padding: const EdgeInsets.only(top: 1.0, left: 55.0),
-      child: Text(
-        cardModel.cardNumber.substring(12),
-        style: TextStyle(color: Colors.white, fontSize: 8.0),
-      ),
-    );
-
-    final _cardValidThru = Padding(
-      padding: const EdgeInsets.only(top: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Text(
-                'Valid',
-                style: TextStyle(color: Colors.white, fontSize: 8.0),
-              ),
-              Text(
-                'thru',
-                style: TextStyle(color: Colors.white, fontSize: 8.0),
-              )
-            ],
-          ),
-          SizedBox(width: 5.0),
-          Text(
-            '${cardModel.cardMonth}/${cardModel.cardYear.substring(2)}',
-            style: TextStyle(color: Colors.white, fontSize: 16.0),
-          )
-        ],
-      ),
-    );
-    
-    final _cardOwner = Padding(
-      padding: const EdgeInsets.only(top: 15.0, left: 50.0),
-      child: Text(
-        cardModel.cardHolderName,
-        style: TextStyle(color: Colors.white, fontSize: 18.0),
-      ),
-    );
-    
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: cardModel.cardColor
-      ),
-      child: RotatedBox(
-        quarterTurns: 3,
+  Widget build(BuildContext context) => Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(15.0),
+      color: cardModel.cardColor
+    ),
+    child: RotatedBox(
+      quarterTurns: MediaQuery.of(context).orientation == Orientation.portrait
+        ? 3 : 0,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            _cardLogo,
-            CardChip(),
-            _cardNumber,
-            _cardLastNumber,
-            _cardValidThru,
-            _cardOwner
+            SizedBox(height: 5),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CardChip(),
+                CardLogo(cardModel.cardType),
+              ]
+            ),
+            SizedBox(height: 40),
+            Wrap(
+              children: List<Widget>.filled(
+                12,
+                dot,
+                growable: true,
+              )
+              ..insert(  // now get the spaces
+                4, dotPadding
+              )
+              ..insert(
+                9, dotPadding
+              )
+              ..add(dotPadding)
+              ..add(
+                Text(
+                  cardModel.cardNumber.substring(12),
+                  style: TextStyle(color: Colors.white),
+                  textScaleFactor: 1.25,
+                )
+              )
+            ),
+            Text(
+              cardModel.cardNumber.substring(12),
+              style: TextStyle(color: Colors.white)
+            ),
+            SizedBox(height: 30),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  cardModel.cardHolderName,
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text (
+                      "Valid\nthru",
+                      style: TextStyle(color: Colors.white),
+                      textScaleFactor: 0.5,
+                      textAlign: TextAlign.end,
+                    ),
+                    SizedBox(width: 5.0),
+                    Text(
+                      '${cardModel.cardMonth}/${cardModel.cardYear.substring(2)}',
+                      style: TextStyle(color: Colors.white),
+                      textScaleFactor: 1.2,
+                    )
+                  ],
+                )
+              ]
+            ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildDots(){
-    List<Widget> dotList = List<Widget>();
-    var counter = 0;
-    for (var i = 0; i < 12; i++) {
-      counter += 1;
-      dotList.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 3.0),
-          child: Container(
-            width: 9.0,
-            height: 9.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle
-            ),
-          ),
-        )
-      );
-      if(counter == 4){
-        counter = 0;
-        dotList.add(SizedBox(width: 50.0));
-      }
-    }
-    dotList.add(_buildNumbers());
-    return Row(children: dotList);
-  }
-
-  Widget _buildNumbers(){
-    return Text(
-        cardModel.cardNumber.substring(12),
-        style: TextStyle(color: Colors.white),
-    );
-  }
+    ),
+  );
 }
