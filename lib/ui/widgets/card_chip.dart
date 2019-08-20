9d9793914c122e98a7bf206cef895578dc29f802
Levi Lesches <levilesches@gmail.com>
2019-08-20 19:33:21 -0400

    Resized CardChip

--- lib/ui/widgets/card_chip.dart
+++ lib/ui/widgets/card_chip.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 
 class CardChip extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Padding(
-      padding: const EdgeInsets.only(top: 10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Image(
-            image: AssetImage('assets/creditcardchipsilver.png'),
-            width: 50.0,
-          ),
-          SizedBox(width: 50.0),
-          SizedBox(width: 50.0),
-          SizedBox(width: 50.0),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.only(top: 30.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Image(
+          image: AssetImage('assets/creditcardchipsilver.png'),
+          width: 50.0,
+        ),
+      ],
+    ),
+  );
 }
